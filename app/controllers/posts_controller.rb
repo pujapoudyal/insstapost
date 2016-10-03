b291a90cc65ed49bb63b50b8ffee75dd349a84d4
Puja <poudyalpuja99@gmail.com>
2016-10-03 09:56:17 +0000

    Post backend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+
     def new
         @post = Post.new
+        
+    end
+    
+    def create
+        @post = current_user_posts.create(post_params)
+        if @post.valid?
+            redirect_to_root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    def post_params
+        params.require(:post).permit(:user_id , :photo , :description)
     end
 end
