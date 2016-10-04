e8d3dcaf694fac1cdf599908ddf2720baa0d7fa3
Puja <poudyalpuja99@gmail.com>
2016-10-04 02:02:38 +0000

    Setting updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,20 @@ class PostsController < ApplicationController
         end
     end
     
+    def edit
+      @post = Post.find(params[:id])
+    end
+    
+    def update
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+      if @post.valid?
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+    
     private
     def post_params
         params.require(:post).permit(:user_id , :photo , :description)
