ae333dfda2d39d430c5878d892e926c2080255db
Puja <poudyalpuja99@gmail.com>
2016-10-08 01:38:01 +0000

    Posting comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+    attr_accessor :content, :name, :title
+        validates :name,  :presence => true
+        validates :title, :presence => true,
+                    :length => { :minimum => 5 }
     has_many :comments, dependent: :destroy
     
     mount_uploader :photo, PhotoUploader
