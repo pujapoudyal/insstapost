9b9ee4216c72ffa47d9492de8ccbbf917c3b0fac
Puja <poudyalpuja99@gmail.com>
2016-11-02 07:38:03 +0000

    sending an ajax request

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,9 +1,14 @@
 class UpvotesController < ApplicationController
-    before_action :authenticate_user
+    before_action :authenticate_user!
     
     def create
-    @post = Post.find(params[:post_id])
-    @post.liked_by current_user
-    redirect_to posts_path
+        @post = Post.find(params[:post_id])
+        @post.liked_by current_user
+        
+        
+        respond_to do |format|
+          format.html {}
+          format.js {}
+        end
     end
 end
